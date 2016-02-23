309573b32724d9479df333014a12a85b11504430
Andrea Bertagnolli <andrea.bertagnolli@paros.tn.it>
2016-02-23 14:39:13 +0100

    Start nginx

--- cookbooks/blog/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/blog/test/integration/default/serverspec/default_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe 'blog::default' do
   describe package('nginx') do
-      it { should be_installed }
+    it { should be_installed }
+  end
+
+  describe service('nginx') do
+    it { should be_enabled }
+    it { should be_running }
   end
 end
