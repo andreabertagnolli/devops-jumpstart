8e338c9353b3ee90526d4650ccfe25319aba8a02
Andrea Bertagnolli <andrea.bertagnolli@paros.tn.it>
2016-02-23 15:00:40 +0100

    Add blog file configuration

--- cookbooks/blog/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/blog/test/integration/default/serverspec/default_spec.rb
@@ -5,8 +5,20 @@ describe 'blog::default' do
     it { should be_installed }
   end
 
+  describe file('/etc/nginx/sites-available/blog.com') do
+    it { should exist }
+  end
+
+  describe file('/etc/nginx/sites-enabled/blog.com') do
+    it { should be_symlink }
+  end
+
   describe service('nginx') do
     it { should be_enabled }
     it { should be_running }
+  end
+
+  describe port(80) do
+    it { should be_listening }
   end
 end
