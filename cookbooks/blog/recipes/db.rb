f75c9322018b47087111e4c96c203d18ae4694b4
Andrea Bertagnolli <andrea.bertagnolli@paros.tn.it>
2016-02-24 09:35:49 +0100

    Extract attributes in file

--- cookbooks/blog/recipes/db.rb
+++ cookbooks/blog/recipes/db.rb
@@ -16,15 +16,15 @@ connection_info = {
   password: ''
 }
 
-mysql_database 'blog' do
+mysql_database node['blog']['database']['name'] do
   connection connection_info
   action :create
 end
 
-mysql_database_user 'blog' do
+mysql_database_user node['blog']['database']['username'] do
   connection connection_info
-  database_name 'blog'
-  password 'blog'
+  database_name node['blog']['database']['name']
+  password node['blog']['database']['password']
   privileges [:all]
   action [:create, :grant]
 end
